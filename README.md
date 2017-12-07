244c387513b3650e781a48fb52216252699ff38b
Christopher Yovanovitch <yovano_c@outlook.com>
2017-12-07 13:01:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# D2Readers
+
+Dofus 2 Data Readers
+
+You need **Yarn** in order to use this project properly.
+```sh
+npm install -g yarn
+```
---
+++ tests/main.test.ts
@@ -0,0 +1,11 @@
+import * as chai from "chai";
+import * as mocha from "mocha";
+
+const expect = chai.expect;
+
+describe("Main", () => {
+
+  it("should be create an instance of d2readers", () => {
+    //
+  });
+});
